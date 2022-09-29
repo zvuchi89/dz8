ce7f08004fea6c2b4e986226917ebebc919feb59
Ekaterina <zvuchi89@mail.ru>
2022-09-29 12:22:01 +0300

    zadacha56

---
+++ zadacha56/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("введите размер  массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+InputArray(matrix);
+PrintArray(matrix);
+int minsum = Int32.MaxValue;
+int k = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum = sum + matrix[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        k++;
+    }
+}
+
+Console.WriteLine("строка с наименьшей суммой элементов под номером: " + (k) + ", с суммой элементов равной: " + (minsum));
+
+void InputArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
